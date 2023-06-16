5bce55454f7c3300b8ecf8a87c24302c7228701d
Jose Segura <jossegura2023@u.northwestern.edu>
2023-06-15 21:58:33 -0500

    added code to footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,9 @@ import React from 'react';
 const Footer = () => {
     return (
         <footer>
-            
+            <a href='https://github.com/Jose-seg' target='_blank' rel='noopener noreferrer'>GitHub</a>
         </footer>
-    )
-}
+    );
+};
+
+export default Footer;

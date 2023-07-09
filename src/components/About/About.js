3f4f34bd86ce5af9103fe10afc620e1db8a1038b
Jose Segura <jossegura2023@u.northwestern.edu>
2023-07-09 18:33:46 -0500

    changed a few things and created code for some components

---
+++ src/components/About/About.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './About.css';
+import myImage from '../../assests/hey-me.jpg';
+
+const About = () => {
+    return (
+        <div className='about-container'>
+            <img src={myImage} alt="The Developer" className='my-image'/>
+            <div className='about-text'>
+                <h2>About Me</h2>
+                <p>
+                Hi, I'm a web developer with experience in building single-page applications using React. 
+                    I have a passion for coding and love to learn new technologies. 
+                    Take a look at my portfolio to see some of the projects I've worked on!
+                </p>
+            </div>
+        </div>
+    );
+}
+
+export default About;

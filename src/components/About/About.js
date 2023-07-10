00a2e36f8289f590675e57ae6d8f4252ca65c8b6
Jose Segura <jossegura2023@u.northwestern.edu>
2023-07-09 22:37:32 -0500

    few changes to improve app

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -8,11 +8,13 @@ const About = () => {
             <img src={myImage} alt="The Developer" className='my-image'/>
             <div className='about-text'>
                 <h2>About Me</h2>
-                <p>
-                    Hi, I'm a web developer with experience in building single-page applications using React. 
-                    I have a passion for coding and love to learn new technologies. 
-                    Take a look at my portfolio to see some of the projects I've worked on!
-                </p>
+                <p>Hello! I'm Jose Segura, an ambitious and aspiring web developer with a keen interest in building interactive and user-friendly web applications. I've always been fascinated with the power of technology and how it allows us to create tools and experiences that are accessible to everyone, everywhere.</p>
+  
+  <p>During my journey into web development, I've been privileged to work with diverse teams, creating a multitude of projects that have challenged me and expanded my skills. On this website, you'll find a portfolio of my most impactful projects, all of which reflect my dedication to learning, growth, and the production of high-quality work.</p>
+  
+  <p>While I've included several team projects here, I am also looking forward to adding my personal projects in the near future. These projects, currently in the making, embody my creativity, personal style, and the depth of knowledge I've acquired during my web development cohort. Stay tuned!</p>
+  
+  <p>I'm always eager to learn, create, and overcome new challenges in the dynamic field of web development. I believe in the potential of technology to make the world a better place, and I can't wait to contribute my part.</p>
             </div>
         </div>
     );
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,7 @@ import './Header.css';
 function Header({ currentSection, setCurrentSection }) {
     return (
         <header>
-            <h1>Jose Segura</h1>
+            <h1>The Code Journey</h1>
             <Navigation 
                 currentSection={currentSection} 
                 setCurrentSection={setCurrentSection}
